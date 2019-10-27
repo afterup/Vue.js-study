f82b55c7c7c34050f06e3569c0022b2e5954c368
fora <afterup@naver.com>
2019-10-27 17:39:27 +0900

    study section7

---
+++ 07 An Introduction to Components/Mission/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+    <!-- <div class="container">
+        <Header></Header>
+        <hr>
+        <div class="row">
+            <Servers></Servers>
+            <ServerDetail></ServerDetail>
+        </div>
+        <hr>
+        <Footer></Footer>
+    </div> -->
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <div class="row">
+            <app-servers></app-servers>
+            <app-server-detail></app-server-detail>
+        </div>
+        <hr>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+    import Header from './components/Shared/Header.vue';
+    import Footer from './components/Shared/Footer.vue';
+    import ServerDetail from './components/Server/ServerDetail.vue';
+    import Servers from './components/Server/Servers.vue';
+
+    export default {
+        components: {
+            'app-header':Header,
+            'app-footer':Footer,
+            'app-server-detail':ServerDetail,
+            'app-servers':Servers,
+        }
+    }
+
+</script>
+
+<style>
+
+</style>
---
+++ 07 An Introduction to Components/Mission/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+// import Header from './Header.vue'
+// import Footer from './Footer.vue'
+// import ServerDetail from './ServerDetail.vue'
+// import Servers from './Servers.vue'
+
+// Vue.component('Header',Header);
+// Vue.component('Footer',Footer);
+// Vue.component('ServerDetail',ServerDetail);
+// Vue.component('Servers',Servers);
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
