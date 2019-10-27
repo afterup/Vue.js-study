53a2a152b77ead56c7bf9b56124a0b0765915d32
fora <afterup@naver.com>
2019-10-27 23:15:27 +0900

    study section8

---
+++ 08 Communicating between Components/Example1/src/components/UserDetail.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>User name : {{ switchName() }}</p>
+        <button @click="resetName">Reset Name</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            name: String,
+        },
+        methods: {
+            switchName(){
+                return this.name.split("").reverse().join("");
+            },
+            resetName(){
+                this.name = 'Max';
+                this.$emit('nameWasReset', this.name);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
