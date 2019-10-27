ee5c8ea608276636f29795c170c31931c77677a0
fora <afterup@naver.com>
2019-10-27 16:44:15 +0900

    study section5,6

---
+++ 07 An Introduction to Components/Exercise1/src/ServerStatus.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p>Server status: {{ status }}</p>
+        <button @click="changeStatus">change</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return {
+            status:'critical',
+        }
+    },    
+    methods: {
+        changeStatus() {
+            this.status = 'change';
+        }
+    },
+}
+</script>
