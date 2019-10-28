194419b58f80bd4f8039128e0f17e0438cc5422e
fora <afterup@naver.com>
2019-10-28 13:01:37 +0900

    study section 9

---
+++ 09/Example/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <button @click="selectedComponent = 'appQuote'">Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New</button>
+                <hr>
+                <keep-alive>
+                    <component :is="selectedComponent">
+                        <p>Default</p>
+                    </component>
+                </keep-alive>
+            <!--
+                <app-quote>
+                    <h1 slot="title">How to ...</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex architecto at, unde ea laboriosam quasi enim sunt eveniet beatae et, aperiam dolores molestias nulla deleniti voluptatum deserunt assumenda neque possimus!</p>
+                </app-quote>
+            -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './components/Quote.vue';
+    import New from './components/New.vue';
+    import Author from './components/Author.vue';
+
+    export default {
+        data:function(){
+            return {
+                quoteTitle: "The Quote",
+                selectedComponent: 'appQuote',
+            }
+        },
+        components: {
+            appQuote:Quote,
+            appNew: New,
+            appAuthor: Author,
+        },
+    }
+</script>
+
+<style>
+</style>
---
+++ 09/Mission/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button class="btn btn-primary" @click="selectedComponent='appBlue'">Load Blue Template</button>
+                <button class="btn btn-success" @click="selectedComponent='appGreen'">Load Green Template</button>
+                <button class="btn btn-danger" @click="selectedComponent='appRed'">Load Red Template</button>
+                <hr>
+                <component :is="selectedComponent">
+                    <p slot="content">{{ content }}</p>
+                </component>
+<!--
+                <app-blue></app-blue>
+                <app-green></app-green>
+                <app-red></app-red>
+-->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        data: function(){
+            return {
+                content: 'this is Content',
+                selectedComponent: 'appBlue',
+            }
+        },
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        }
+    }
+</script>
+
+<style>
+</style>
