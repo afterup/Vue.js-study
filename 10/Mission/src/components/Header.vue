a85de5a13a8ea9a6055ff3dd0b538cbf4816fcdf
fora <afterup@naver.com>
2019-10-28 15:26:40 +0900

    study section10

---
+++ 10/Mission/src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <div class="progress">
+            <div class="progress-bar" role="progressbar" :style="{width: (quoteCount / maxQuotes) * 100 +'%'}" aria-valuenow="25" aria-valuemin="0"
+                aria-valuemax="100">{{ quoteCount }} / {{ maxQuotes }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['quoteCount', 'maxQuotes'],
+    }
+</script>
---
+++ 10/Mission/src/components/QuoteGrid.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <app-quote v-for="(quote,index) in quotes" 
+                    :key="quote.id"
+                    @click.native="deleteQuote(index)">{{ quote }}</app-quote>
+    </div>
+</template>
+
+<script>
+    import Quote from './Quote.vue'
+
+    export default {
+        props: ['quotes'],
+        components: {
+            appQuote: Quote,
+        },
+        methods: {
+            deleteQuote(index){
+                console.log(index);
+                this.$emit('quoteDeleted',index);
+            }
+        }
+    }
+</script>
