a77114ab690ab819c15ba09ec596eb5df132904c
fora <afterup@naver.com>
2019-10-28 21:13:17 +0900

    study section12 And rename folders

---
+++ 12 Using and Creating Directives/Mission/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Directives Exercise</h1>
+                <!-- Exercise -->
+                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+                <button class="btn btn-primary" v-myon:click="clicked">Click</button>
+                <div style="width:50px; height:50px; background-color:gray;"
+                     v-myon:mouseenter="mouseEnter"
+                     v-myon:mouseleave="mouseLeave"></div> 
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        directives: {
+            'myon': {
+                bind(el, binding){
+                    const type = binding.arg;
+                    const fn = binding.value;
+                    el.addEventListener(type,fn);
+                }
+            }
+        },
+        methods: {
+            clicked(){
+                alert('Click');
+            },
+            mouseEnter(){
+                console.log('Enter');
+            },
+            mouseLeave(){
+                console.log('Leave');
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
