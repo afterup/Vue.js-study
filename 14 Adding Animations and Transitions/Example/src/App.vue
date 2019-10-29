be2e7e0d379ea47a54dc14486d4f0f708f3dbdde
fora <afterup@naver.com>
2019-10-29 15:02:41 +0900

    study section14-1

---
+++ 14 Adding Animations and Transitions/Example/src/App.vue
@@ -0,0 +1,170 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Animations</h1>
+                <hr>
+                <select v-model="alertAnimation">
+                    <option value="fade" selected>Fade</option>
+                    <option value="slide">Slide</option>
+                </select>
+                <button class="btn btn-primary" @click="show = !show">Show Alert</button>
+                <br><br>
+                <transition :name="alertAnimation">
+                    <div class="alert alert-info" v-if="show">This is Alert Info</div>
+                </transition>
+                <transition :name="alertAnimation" type="animation">
+                    <div class="alert alert-info" v-if="show">This is Alert Info</div>
+                </transition>
+                <transition :name="alertAnimation" appear>
+                    <div class="alert alert-info" v-if="show">This is Alert Info</div>
+                </transition>
+                <transition 
+                            enter-active-class="animated bounce"
+                            leave-active-class="animated jello">
+                    <div class="alert alert-info" v-if="show">This is Alert Info</div>
+                </transition>
+                <transition :name="alertAnimation" mode="out-in">
+                    <div class="alert alert-info" v-if="show" key="info">This is Alert Info</div>
+                    <div class="alert alert-warning" v-else key="warning">This is Alert Info</div>
+                </transition>
+                <hr>
+                <!-- JavaScript Animation-->
+                <button class="btn btn-primary" @click="load = !load"> Load / Remove Element </button>
+                <br>
+                <br>
+                <transition 
+                            @before-enter="beforeEnter"
+                            @enter="enter"
+                            @after-enter="afterEnter"
+                            @enter-cancelled="enterCancelled"
+
+                            @before-leave="beforeLeave"
+                            @leave="leave"
+                            @after-leave="afterLeave"
+                            @leave-cancelled="leaveCancelled"
+                            :css="false">
+                    <div style="background-color:green; width:50px; height:50px;" v-if="load"></div>
+                </transition>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                show : true,
+                load:true,
+                alertAnimation: 'fade',
+                elementWidth: 100,
+            }
+        },
+        methods: {
+            beforeEnter: (el) => {
+               console.log('beforeEnter');
+               this.elementWidth = 100;
+               el.style.width = this.elementWidth + 'px';
+            },
+            enter(el, done){
+                console.log('enter');
+                let round = 1;
+                const interval = setInterval(() => {
+                    el.style.width = (this.elementWidth + round*10) + 'px';
+                    round++;
+                    if( round > 20){
+                        clearInterval(interval);
+                        done();
+                    }
+                },20);
+            },
+            afterEnter(el){
+                console.log('afterEnter');
+            },
+            enterCancelled(el){
+                console.log('enterCancelled');
+            },
+            beforeLeave(el){
+                console.log('beforeLeave');
+                this.elementWidth = 300;
+                el.style.width = this.elementWidth + 'px';
+            },
+            leave(el,done){
+                console.log('leave');
+                let round = 1;
+                const interval = setInterval(() => {
+                    el.style.width = (this.elementWidth - round*10) + 'px';
+                    round++;
+                    if( round > 20){
+                        clearInterval(interval);
+                        done();
+                    }
+                },20);
+            },
+            afterLeave(el){
+                console.log('afterleave');
+            },
+            leaveCancelled(el){
+                console.log('leaveCancelled');
+            }
+        }
+    }
+</script>
+
+<style>
+    .fade-enter {
+        opacity:0;
+    }
+
+    .fade-enter-active {
+        transition: opacity 1s;
+    }
+
+    .fade-leave {
+        /*opacity: 1;*/
+    }
+
+    .fade-leave-active{
+        transition: opacity 1s;
+        opacity: 0;
+    }
+
+    .slide-enter{
+        opacity: 0;
+    }
+
+    .slide-enter-active{
+        animation: slide-in 1s ease-out forwards;
+        transition: opacity .5s;
+    }
+
+    .slide-leave{
+
+    }
+
+    .slide-leave-active{
+        animation: slide-out 1s ease-out forwards;
+        transition: opacity 3s;
+        opacity: 0;
+    }
+
+    @keyframes slide-in {
+        from {
+            transform : translateY(20px);
+        }
+        to {
+            transform: translateY(0);
+        }
+    }
+
+    @keyframes slide-out {
+        from {
+            transform : translateY(0);
+        }
+        to {
+            transform: translateY(20px);
+        }
+    }
+
+</style>
