a503a6a338ef3b0903e1752a3c0b6f0ba32de8d5
fora <afterup@naver.com>
2019-10-29 20:37:32 +0900

    study section16-1

---
+++ 16 Routing in a VueJS Application/Example/src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <ul class="nav nav-pills">
+      <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
+      <router-link to="/user/1" tag="li" active-class="active"><a>User1</a></router-link>
+      <router-link to="/user/2" tag="li" active-class="active"><a>User2</a></router-link>
+    </ul>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
