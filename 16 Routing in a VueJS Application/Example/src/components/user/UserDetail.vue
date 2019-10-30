8cdc055831c154dd3e4caa7f3789caf1c895cb39
fora <afterup@naver.com>
2019-10-30 17:11:11 +0900

    study section 16-2

--- 16 Routing in a VueJS Application/Example/src/components/user/UserDetail.vue
+++ 16 Routing in a VueJS Application/Example/src/components/user/UserDetail.vue
@@ -3,8 +3,30 @@
         <h3>Some User Details</h3>
         <p>ID: {{ $route.params.id }}</p>
         <router-link tag="button" 
-                     :to="{ name:'userEdit',params:{ id:$route.params.id }, query:{ locale:'en', key:'one'}}"
+                     :to="link"
                      class="btn btn-warning"> Edit </router-link>
                 
     </div>
 </template>
+
+<script>
+    export default {
+        data(){
+            return {
+                link: { 
+                    name:'userEdit',
+                    params:{ id:this.$route.params.id }, 
+                    query:{ locale:'en', key:'one'},
+                    hash: "#data"
+                }
+            }
+        },
+        beforeRouteEnter(to, from, next){
+            if(true){
+                next();
+            }else{
+                next(false);
+            }
+        }
+    }
+</script>
--- 16 Routing in a VueJS Application/Example/src/components/user/UserEdit.vue
+++ 16 Routing in a VueJS Application/Example/src/components/user/UserEdit.vue
@@ -3,5 +3,24 @@
        <h3>Edit the User</h3>
         <p> locale: {{ $route.query.locale }}</p>
         <p> key : {{ $route.query.key }}</p>
+        <div style="height: 700px"></div>
+        <p id="data">some</p>
     </div>
 </template>
+
+<script>
+    export default {
+        data(){
+            return {
+                confirmed: false
+            }
+        },
+        beforeRouteLeave (to, from, next) {
+            if(this.confirmed){
+                next();
+            } else {
+                confirm('Sure?')?next():next(false);
+            }
+        }
+    }
+</script>
